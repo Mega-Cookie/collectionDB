b6dcd6314aa3e352acfc5f2af259e0c573c92ac1
Lucas Koop <lucas.koop@sectornord.de>
2025-09-30 16:52:46 +0200

    rework datastruct

--- entries/main.go
+++ entries/main.go
@@ -2,6 +2,7 @@ package entries
 
 import (
 	"collectionDB/collect"
+	"collectionDB/stockdata"
 	"database/sql"
 	"fmt"
 	"net/http"
@@ -10,16 +11,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var db *sql.DB
-
 type Entry struct {
-	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Plot      string    `json:"plot"`
-	Medium    string    `json:"medium"`
-	Year      int       `json:"year"`
-	CollID    string    `json:"collid"`
-	CollName  string    `json:"collname"`
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Plot  string `json:"plot"`
+	Year  int    `json:"year"`
+	Type  struct {
+		ID   int    `json:"typeid"`
+		Name string `json:"typename"`
+	}
+	Collection struct {
+		ID   int    `json:"collid"`
+		Name string `json:"collname"`
+	}
+	Genre struct {
+		ID   int    `json:"genreid"`
+		Name string `json:"genrename"`
+	}
 	CreatedAt time.Time `json:"created_at"`
 	EditedAt  time.Time `json:"edited_at"`
 	IsDigital bool      `json:"is_digital"`
@@ -35,13 +43,14 @@ func ShowCreateEntryPage(db *sql.DB) gin.HandlerFunc {
 }
 func CreateEntry(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		title := c.PostForm("title")
-		plot := c.PostForm("plot")
-		medium := c.PostForm("medium")
-		year := c.PostForm("year")
-		collid := c.PostForm("collid")
-		isDigital := c.PostForm("is_digital") == "on"
-		_, err := db.Exec(`INSERT INTO entries (TITLE, YEAR, PLOT, MEDIUM, IS_DIGITAL, collectionID) VALUES (?, ?, ?, ?, ?, ?)`, title, year, plot, medium, isDigital, collid)
+		Title := c.PostForm("title")
+		Plot := c.PostForm("plot")
+		Typeid := c.PostForm("typeid")
+		Genreid := c.PostForm("genreid")
+		Year := c.PostForm("year")
+		Collid := c.PostForm("collid")
+		IsDigital := c.PostForm("is_digital") == "on"
+		_, err := db.Exec(`INSERT INTO entries (TITLE, YEAR, PLOT, TypeID, GenreID, IS_DIGITAL, collectionID) VALUES (?, ?, ?, ?, ?, ?)`, Title, Year, Plot, Typeid, Genreid, IsDigital, Collid)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create entry"})
 			return
@@ -51,15 +60,16 @@ func CreateEntry(db *sql.DB) gin.HandlerFunc {
 }
 func EditEntry(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		title := c.PostForm("title")
-		plot := c.PostForm("plot")
-		medium := c.PostForm("medium")
-		year := c.PostForm("year")
-		collection := c.PostForm("collid")
-		isDigital := c.PostForm("is_digital") == "on"
+		Title := c.PostForm("title")
+		Plot := c.PostForm("plot")
+		Typeid := c.PostForm("typeid")
+		Genreid := c.PostForm("genreid")
+		Year := c.PostForm("year")
+		Collid := c.PostForm("collid")
+		IsDigital := c.PostForm("is_digital") == "on"
 		id := c.Param("id")
-		updateTableQuery := `UPDATE entries SET TITLE = ?, YEAR = ?, PLOT = ?, MEDIUM = ?, IS_DIGITAL = ?, collectionID = ?, EDITED_AT = CURRENT_TIMESTAMP where entryID = ?`
-		_, err := db.Exec(updateTableQuery, title, year, plot, medium, isDigital, collection, id)
+		updateTableQuery := `UPDATE entries SET TITLE = ?, YEAR = ?, PLOT = ?, TypeID = ?, GenreID = ?, IS_DIGITAL = ?, collectionID = ?, , EDITED_AT = CURRENT_TIMESTAMP where entryID = ?`
+		_, err := db.Exec(updateTableQuery, Title, Year, Plot, Typeid, Genreid, IsDigital, Collid, id)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to edit entry"})
 			return
@@ -75,7 +85,7 @@ func ListEntries(db *sql.DB) (entries []Entry) {
 	defer rows.Close()
 	for rows.Next() {
 		var entry Entry
-		rows.Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.Medium, &entry.IsDigital, &entry.CollID, &entry.CreatedAt, &entry.EditedAt, &entry.CollName)
+		rows.Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.Type.ID, &entry.IsDigital, &entry.Collection.ID, entry.Genre.ID, entry.Type.ID, &entry.CreatedAt, &entry.EditedAt, &entry.Collection.Name, &entry.Genre.Name, &entry.Type.Name)
 		entries = append(entries, entry)
 	}
 	return
@@ -95,8 +105,8 @@ func PreviewSharedEntry(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		var entry Entry
-		query := "SELECT entryID, TITLE, YEAR, PLOT, MEDIUM, collectionID, CREATED_AT, EDITED_AT FROM entries WHERE id = ?"
-		err := db.QueryRow(query, id).Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.Medium, &entry.CollName, &entry.CreatedAt, &entry.EditedAt)
+		query := "SELECT entryID, TITLE, YEAR, PLOT, IS_DIGITAL, collectionID GenreID, TypeID, CREATED_AT, EDITED_AT FROM entries WHERE id = ?"
+		err := db.QueryRow(query, id).Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.IsDigital, &entry.Collection.ID, entry.Genre.ID, entry.Type.ID, &entry.CreatedAt, &entry.EditedAt)
 		if err != nil {
 			c.HTML(http.StatusNotFound, "404.html", nil)
 			return
@@ -106,11 +116,13 @@ func PreviewSharedEntry(db *sql.DB) gin.HandlerFunc {
 }
 func ShowEditEntryPage(db *sql.DB) gin.HandlerFunc {
 	collections := collect.ListCollections(db)
+	genres := stockdata.ListGenres(db)
+	mediatypes := stockdata.ListMediatypes(db)
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		var entry Entry
 		query := "SELECT e.*, c.NAME AS COLLNAME FROM `entries` e JOIN collections c on c.collectionID = e.collectionID WHERE entryID = ?"
-		err := db.QueryRow(query, id).Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.Medium, &entry.IsDigital, &entry.CollID, &entry.CreatedAt, &entry.EditedAt, &entry.CollName)
+		err := db.QueryRow(query, id).Scan(&entry.ID, &entry.Title, &entry.Year, &entry.Plot, &entry.IsDigital, &entry.Collection.ID, entry.Genre.ID, entry.Type.ID, &entry.CreatedAt, &entry.EditedAt, &entry.Collection.Name, &entry.Genre.Name, &entry.Type.Name)
 		if err != nil {
 			c.HTML(http.StatusNotFound, "404.html", nil)
 			return
@@ -118,6 +130,8 @@ func ShowEditEntryPage(db *sql.DB) gin.HandlerFunc {
 		c.HTML(http.StatusOK, "edit_entry.html", gin.H{
 			"Entry":       entry,
 			"Collections": collections,
+			"Genres":      genres,
+			"Mediatypes":  mediatypes,
 		})
 	}
 }
