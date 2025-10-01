c629d2070fe64931f7226a084f5170e3941a36b8
Lucas Koop <lucas.koop@sectornord.de>
2025-10-01 09:04:14 +0200

    Added View

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func initDB() {
 		log.Fatal(err)
 	}
 	createTableQuery = `CREATE TABLE IF NOT EXISTS genres (
-		GenreID INTEGER PRIMARY KEY AUTOINCREMENT,
+		genreID INTEGER PRIMARY KEY AUTOINCREMENT,
 		NAME STRING UNIQUE
 	);`
 	_, err = db.Exec(createTableQuery)
@@ -106,7 +106,7 @@ func initDB() {
 		log.Fatal(err)
 	}
 	createTableQuery = `CREATE TABLE IF NOT EXISTS categories (
-		CategoryID INTEGER PRIMARY KEY AUTOINCREMENT,
+		categoryID INTEGER PRIMARY KEY AUTOINCREMENT,
 		NAME STRING UNIQUE
 	);`
 	_, err = db.Exec(createTableQuery)
@@ -120,12 +120,12 @@ func initDB() {
 		PLOT TEXT NOT NULL,
 		IS_DIGITAL BOOL NOT NULL DEFAULT 0,
 		collectionID INTEGER DEFAULT NULL,
-		GenreID INT DEFAULT NULL,
+		genreID INT DEFAULT NULL,
 		typeID INTEGER DEFAULT NULL,
 		CREATED_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
 		EDITED_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
 		FOREIGN KEY(collectionID) REFERENCES collections(collectionID),
-		FOREIGN KEY(GenreID) REFERENCES genres(GenreID),
+		FOREIGN KEY(genreID) REFERENCES genres(genreID),
 		FOREIGN KEY(typeID) REFERENCES mediatypes(typeID)
 	);`
 	_, err = db.Exec(createTableQuery)
@@ -136,10 +136,10 @@ func initDB() {
 		collectionID INTEGER PRIMARY KEY AUTOINCREMENT,
 		NAME TEXT UNIQUE,
 		DESCRIPTION TEXT,
-		CategoryID INT DEFAULT NULL,
+		categoryID INT DEFAULT NULL,
 		CREATED_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
 		EDITED_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
-		FOREIGN KEY(CategoryID) REFERENCES categories(CategoryID)
+		FOREIGN KEY(categoryID) REFERENCES categories(categoryID)
 	);`
 	_, err = db.Exec(createTableQuery)
 	if err != nil {
@@ -181,12 +181,13 @@ func main() {
 	router.GET("/entries/:id/edit", entries.ShowEditEntryPage(db))
 	router.POST("/entries/:id/edit", entries.EditEntry(db))
 	router.POST("/entries/:id/delete", entries.DeleteEntry(db))
-	router.GET("/entries/:id", entries.PreviewSharedEntry(db))
+	router.GET("/entries/:id", entries.ViewEntry(db))
 	router.GET("/create_collection", collect.ShowCreateCollectionPage(db))
 	router.POST("/create_collection", collect.CreateCollection(db))
 	router.GET("/collections/:id/edit", collect.ShowEditCollectionPage(db))
 	router.POST("/collections/:id/edit", collect.EditCollection(db))
 	router.POST("/collections/:id/delete", collect.DeleteCollection(db))
+	router.GET("/collections/:id", collect.ViewCollection(db))
 	port := ":8080"
 	log.Printf("Server is running on http://localhost%s", port)
 	if err := router.Run(port); err != nil {
