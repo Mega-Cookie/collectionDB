481f384d02a4e06c747d8c0cc1e3cf84112fb642
Mega-Cookie <lcs.kp@protonmail.com>
2025-10-05 22:35:14 +0200

    add about

--- small/main.go
+++ small/main.go
@@ -28,6 +28,15 @@ type config struct {
 	Templates string `json:"templates"`
 	IsDebug   bool   `json:"debug"`
 }
+type systeminfo struct {
+	ID        int    `json:"id"`
+	Version   string `json:"version"`
+	Hostname  string `json:"hostname"`
+	OS        string `json:"os"`
+	Arch      string `json:"arch"`
+	GoVersion string `json:"go_version"`
+	Timezone  string `json:"tzone"`
+}
 
 func Configure() (config config) {
 	filename := flag.String("config", "config.yml", "")
@@ -89,15 +98,19 @@ func SetTime(db *sql.DB, stamp *time.Time) (newstamp time.Time) {
 	err := db.QueryRow(query).Scan(&zone)
 	if err != nil {
 		fmt.Println("error: Failed to get timezone")
-		fmt.Println(err)
 		return
 	}
 	location, err = time.LoadLocation(zone)
 	if err != nil {
 		fmt.Println("error: Failed to set timezone")
-		fmt.Println(err)
 		return
 	}
 	newstamp = stamp.In(location)
+	return
+}
+func GetSystemInfo(db *sql.DB) (systeminfo systeminfo) {
+	query := "SELECT * FROM info WHERE instanceID = 1"
+	response := db.QueryRow(query)
+	response.Scan(&systeminfo.ID, &systeminfo.Version, &systeminfo.Hostname, &systeminfo.OS, &systeminfo.Arch, &systeminfo.GoVersion, &systeminfo.Timezone)
 	return
 }
