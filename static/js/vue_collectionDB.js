a8b106f43922b5d306ecba23ca3c0a10f405a348
Mega-Cookie <Lcs.Kp@protonmail.com>
2026-02-26 13:28:09 +0100

    add stock functions

--- static/js/vue_collectionDB.js
+++ static/js/vue_collectionDB.js
@@ -53,16 +53,20 @@ createApp({
         async deletething(id, name, type) {
             if (!confirm(`Realy delete ${type} ${name}?`)) return;
                 try {
-                    const response = await fetch(`/${type}/${id}/delete`, {
+                    const response = await fetch(`/api/v1/${type}/${id}`, {
                         method: 'DELETE',
                         headers: { 'Content-Type': 'application/json' }
                     });
                     if (response.ok) {
                         const data = await response.json();
                         console.log("Answer recieved:", data);
-                        showSuccessToast(data.message); 
-                        this.fetchCollections();
-                        this.fetchEntries();
+                        showSuccessToast(data.message);
+                        if (type === "collection") {
+                            this.fetchCollections();
+                        } 
+                        else if (type === "entry") {
+                            this.fetchEntries();
+                        }
                     }
                     else {
                             const data = await response.json();
@@ -102,6 +106,68 @@ createApp({
         this.fetchAbout();
     }
 }) .mount('#about');
+createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            MediaTypes: [],
+            CaseTypes: []
+        }
+    },
+    methods: {
+        async fetchMediaTypes() {
+            try {
+                const response = await fetch('/api/v1/mediatypes');
+                const data = await response.json();
+                this.MediaTypes = data.data.MediaTypes || [];
+            } catch (error) {
+                console.error("Fehler beim Laden der Media Types:", error);
+            }
+        },
+        async fetchCaseTypes() {
+            try {
+                const response = await fetch('/api/v1/casetypes');
+                const data = await response.json();
+                this.CaseTypes = data.data.CaseTypes || [];
+            } catch (error) {
+                console.error("Fehler beim Laden der Case Types:", error);
+            }
+        },
+        async deletething(id, name, type) {
+            if (!confirm(`Realy delete ${type} ${name}?`)) return;
+                try {
+                    const response = await fetch(`/api/v1/${type}/${id}`, {
+                        method: 'DELETE',
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                    if (response.ok) {
+                        const data = await response.json();
+                        console.log("Answer recieved:", data);
+                        showSuccessToast(data.message);
+                        if (type === "casetype") {
+                            this.fetchCaseTypes();
+                        } 
+                        else if (type === "mediatype") {
+                            this.fetchMediaType();
+                        }
+                    }
+                    else {
+                            const data = await response.json();
+                            console.log("Answer recieved:", data);
+                            showErrorToast(data.error);
+                        }
+                    }
+                catch (err) {
+                    console.error("Error:", err);
+                }
+        }
+    },
+    mounted() {
+        this.fetchMediaTypes();
+        this.fetchCaseTypes();
+    }
+}) .mount('#stock');
+
 document.addEventListener('click', async function(e) {
     if (e.target.classList.contains('delete-stock-btn')) {
         const type = e.target.getAttribute('data-type')
